4237054733656d2c50ac7b63b6ec143f6bca3f2b
gjinie <yaf2009@naver.com>
2026-01-12 16:04:47 +0900

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,12 @@
 
 import React from 'react';
+import { View } from '../types';
+/* 1. Props 인터페이스 정의 */
+interface FooterProps {
+    setView: (view: View) => void;
+}
 
-const Footer: React.FC = () => {
+const Footer: React.FC<FooterProps> = ({ setView }) => {
   return (
     <footer className="py-12 px-6 border-t border-white/10 mt-auto">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-8">
